7bde2368455f700829a6a53356eefdf70d4fc06e
Vuong Phan <pasv3012@gmail.com>
2026-07-12 11:05:13 +0700

    remove redundant code

--- app/tetris/reducer.ts
+++ app/tetris/reducer.ts
@@ -62,7 +62,6 @@ export function gameReducer(state: GameState, action: Action): GameState {
     case "UP": {
       const nextShape = rotateShape(state.block.shape);
       const nextBlock = { ...state.block, shape: nextShape };
-      nextBlock.shape = rotateShape(state.block.shape);
       if (isCollide(nextBlock, state.grid)) {
         return state;
       }
